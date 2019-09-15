69c6a451dd0c3cb2828ec7b3d2115cb6092300ca
nguyenduy7474 <nguyenduy7474@gmail.com>
2019-09-15 19:15:42 +0700

    ss

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    'url' : 'mongodb://127.0.0.1/webrtc' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-     //'url' : 'mongodb://nguyenduy7475:duy123654789@ds349175.mlab.com:49175/webrtc'
+    //'url' : 'mongodb://127.0.0.1/webrtc' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://nguyenduy7475:duy123654789@ds349175.mlab.com:49175/webrtc'
      //Please replace your host file Here : 127.1.1.0 , Express is Collection Name (Database Name)
 };
