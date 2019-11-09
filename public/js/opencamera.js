82cc1ac7b1393f5eddbc10270e0b77bf0143f89e
nguyenduy7474 <nguyenduy7474@gmail.com>
2019-11-10 01:31:59 +0700

    test191

--- public/js/opencamera.js
+++ public/js/opencamera.js
@@ -1,6 +1,6 @@
-//var socket = io("https://testrtcduy.herokuapp.com")
+var socket = io("https://testrtcduy.herokuapp.com")
 //var socket = io("localhost:3000")
-var socket = io("https://803a6927.ngrok.io")
+//var socket = io("https://803a6927.ngrok.io")
 
 
 navigator.getUserMedia = navigator.getUserMedia ||
